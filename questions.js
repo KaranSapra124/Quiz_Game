e77e5a70e3d68f7a3ba535f09e3358d28960aefa
Karan sapra <88654249+KaranSapra124@users.noreply.github.com>
2024-04-17 10:55:56 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,154 @@
+const Quizes = [
+  {
+    Question: "What is full form of HTML?",
+    Answer: [
+      {
+        option1: "HyperText Markup Language",
+        isTrue: true,
+      },
+      {
+        option2: "HyperText Market Language",
+        isTrue: false,
+      },
+      {
+        option3: "HyperText Mockup Language",
+        isTrue: false,
+      },
+      {
+        option4: "Hyper Markup Language",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    Question: "What does CSS stand for?",
+    Answer: [
+      {
+        option1: "Cascading Style Sheets",
+        isTrue: true,
+      },
+      {
+        option2: "Creative Style Sheets",
+        isTrue: false,
+      },
+      {
+        option3: "Computer Style Sheets",
+        isTrue: false,
+      },
+      {
+        option4: "Colorful Style Sheets",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    Question:
+      "Which programming language is known as the 'language of the web'?",
+    Answer: [
+      {
+        option1: "JavaScript",
+        isTrue: true,
+      },
+      {
+        option2: "Java",
+        isTrue: false,
+      },
+      {
+        option3: "Python",
+        isTrue: false,
+      },
+      {
+        option4: "C++",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    Question: "What is the output of 'console.log(3 + 2 + '7')' in JavaScript?",
+    Answer: [
+      {
+        option1: "327",
+        isTrue: true,
+      },
+      {
+        option2: "12",
+        isTrue: false,
+      },
+      {
+        option3: "57",
+        isTrue: false,
+      },
+      {
+        option4: "NaN",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    Question: "How do you link a CSS file to your HTML file?",
+    Answer: [
+      {
+        option1: "<link rel='stylesheet' href='styles.css'>",
+        isTrue: true,
+      },
+      {
+        option2: "<style src='styles.css'></style>",
+        isTrue: false,
+      },
+      {
+        option3: "<css src='styles.css'></css>",
+        isTrue: false,
+      },
+      {
+        option4: "<link style='styles.css'>",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    Question: "What is the correct way to include JavaScript in an HTML file?",
+    Answer: [
+      {
+        option1: "<script src='script.js'></script>",
+        isTrue: true,
+      },
+      {
+        option2: "<javascript src='script.js'></javascript>",
+        isTrue: false,
+      },
+      {
+        option3: "<script href='script.js'></script>",
+        isTrue: false,
+      },
+      {
+        option4: "<link rel='javascript' href='script.js'>",
+        isTrue: false,
+      },
+    ],
+  },
+  {
+    Question:
+      "What is the purpose of the 'document.getElementById()' method in JavaScript?",
+    Answer: [
+      {
+        option1: "To select an HTML element by its id",
+        isTrue: true,
+      },
+      {
+        option2: "To select all HTML elements with a specific class",
+        isTrue: false,
+      },
+      {
+        option3: "To select the first HTML element in the document",
+        isTrue: false,
+      },
+      {
+        option4: "To select the parent element of an HTML element",
+        isTrue: false,
+      },
+    ],
+  },
+  // Add more questions here...
+];
+
+
